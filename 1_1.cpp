b9a7d0547c05a5f08e84e899772aeaab63b0df40
JiaHui1019 <18226565098@163.com>
2021-01-31 22:27:04 +0800

    2021/1/31

---
+++ 1_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+int main(void)
+{
+    int x = 5, y = 10;
+    cout << "x = " << x << " y = " << y << endl;
+    swap(x, y);
+    cout << "x = " << x << " y = " << y << endl;
+    return 0;
+}
